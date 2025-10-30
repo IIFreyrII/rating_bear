41b1defc5b25e25d7f0e0bc4608ff1249f882e13
Bryan Freyre <freyrecanbryan@gmail.com>
2025-10-29 19:13:07 -0600

    rating stars added

---
+++ android/app/src/main/kotlin/com/example/screen_with_animation/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.screen_with_animation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screens/rating_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+import 'dart:async';
+import 'package:flutter_rating_stars/flutter_rating_stars.dart';
+
+class RatingBear extends StatefulWidget {
+  const RatingBear({super.key});
+
+  @override
+  State<RatingBear> createState() => _RatingBearState();
+}
+
+class _RatingBearState extends State<RatingBear> {
+  
+}
